83d2ef5172526025e37d32217aeb71e40588fda2
DantesDev <lord.atos0411@gmail.com>
2024-12-10 15:41:57 -0500

    init

---
+++ static/js/757.81c7ed13.chunk.js
@@ -0,0 +1,3 @@
+(self.webpackChunkdapp=self.webpackChunkdapp||[]).push([[757,432],{99432:()=>{},20919:()=>{},10833:()=>{},73776:()=>{}}]);
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="8f7d139e-9144-5b73-bac0-843bdf1fdfe3")}catch(e){}}();
+//# debugId=8f7d139e-9144-5b73-bac0-843bdf1fdfe3
